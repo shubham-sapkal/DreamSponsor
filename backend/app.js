1a9a47c153ccb24f5361a33722793c636b51e77d
shubham-sapkal <sapkal.shubham2002@gmail.com>
2023-10-03 23:58:29 +0530

    cors connnection updated

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,15 @@ export const app = express();
 
 import cookieParser from 'cookie-parser';
 
+import cors from "cors";
+
 // user defined routers
 import userRouter from './routes/user.js';
 import postRouter from './routes/post.js';
 
 // Configuring dotenv file
 import { config } from "dotenv";
+import { errorMiddleware } from "./middlewares/error.js";
 config({
     path: './data/config.env'
 });
@@ -16,6 +19,12 @@ config({
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
 
 
 // app.get("/", (req, res) => {
@@ -26,3 +35,6 @@ app.use(cookieParser());
 // using user define routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
+
+// Error Handling middleware
+app.use(errorMiddleware);
